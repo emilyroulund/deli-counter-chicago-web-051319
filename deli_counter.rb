d0792dd5b7ec69beaa6ec3cb19f79b66131b76ae
Emily R <emilyroulund@gmail.com>
2019-04-19 02:49:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line (deli)
-  current_line =[]
+  current_line ="The line is currently: "
   if deli.size == 0 
     puts  "The line is currently empty." 
   elsif i < deli.length
     i = 0 
-    current_line << "The line is currently: #{ i + 1}. #{deli.i}" 
+    current_line <<  "#{ i + 1}. #{deli.i}" 
     i += 1
 end 
 return current_line
